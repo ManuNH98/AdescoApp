c04c25c362d094852f61e065985a2592631049d6
Manuel <manuelnhc@gmail.com>
2021-04-16 12:40:10 +0100

    tercer commit

---
+++ app/src/main/java/com/navarromanuel/adescoapp/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.navarromanuel.adescoapp;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int duracion = 3000;
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable(){
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },duracion);
+    }
+}
