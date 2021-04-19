1a9d50ad2c891b2a82aaf146318f155b5b6905af
Manuel <manuelnhc@gmail.com>
2021-04-19 13:40:28 +0100

    sexto commit

--- app/src/main/java/com/navarromanuel/adescoapp/activity/LoginActivity.java
+++ app/src/main/java/com/navarromanuel/adescoapp/activity/LoginActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.navarromanuel.adescoapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private EditText editTextTextPassword;
+    private CheckBox mostrarContraseña;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        editTextTextPassword = (EditText)findViewById(R.id.editTextTextPassword);
+        mostrarContraseña = (CheckBox)findViewById(R.id.checkBox);
 
     }
 
@@ -26,5 +33,18 @@ public class LoginActivity extends AppCompatActivity {
     public void enviarRegistrar(View view) {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    public void mostrarContraseña(View v){
+        int cursor = editTextTextPassword.getSelectionEnd();
+
+        if(mostrarContraseña.isChecked()){
+            editTextTextPassword.setInputType(InputType.TYPE_CLASS_TEXT
+                    | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+        }else{
+            editTextTextPassword.setInputType(InputType.TYPE_CLASS_TEXT
+                    | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        }
+        editTextTextPassword.setSelection(cursor);
     }
 }
--- app/src/main/java/com/navarromanuel/adescoapp/activity/MenuActivity.java
+++ app/src/main/java/com/navarromanuel/adescoapp/activity/MenuActivity.java
@@ -21,7 +21,7 @@ public class MenuActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_parcela, R.id.navigation_calendario, R.id.navigation_cuaderno)
+                R.id.navigation_parcela, R.id.navigation_calendario, R.id.navigation_cuaderno, R.id.navigation_almacen, R.id.navigation_perfil)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
