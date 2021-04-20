b32d1637110f4ac3b376e0c87f42c4d553e49b76
Manuel <manuelnhc@gmail.com>
2021-04-20 12:43:24 +0100

    octavo commit

--- app/src/main/java/com/navarromanuel/adescoapp/ui/almacen/AlmacenFragment.java
+++ app/src/main/java/com/navarromanuel/adescoapp/ui/almacen/AlmacenFragment.java
@@ -2,65 +2,36 @@ package com.navarromanuel.adescoapp.ui.almacen;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.navarromanuel.adescoapp.R;
+import com.navarromanuel.adescoapp.ui.calendario.CalendarioViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AlmacenFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AlmacenFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private AlmacenViewModel almacenViewModel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AlmacenFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AlmacenFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AlmacenFragment newInstance(String param1, String param2) {
-        AlmacenFragment fragment = new AlmacenFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_almacen, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        almacenViewModel =
+                new ViewModelProvider(this).get(AlmacenViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_almacen, container, false);
+        final TextView textView = root.findViewById(R.id.text_almacen);
+        almacenViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
     }
 }
