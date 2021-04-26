f50a5e971cf60bc3a8f9356a0dcef6c3ec762c0a
Manuel <manuelnhc@gmail.com>
2021-04-26 12:23:04 +0100

    menu commit

--- app/src/main/java/com/navarromanuel/adescoapp/activity/MenuActivity.java
+++ app/src/main/java/com/navarromanuel/adescoapp/activity/MenuActivity.java
@@ -1,15 +1,23 @@
 package com.navarromanuel.adescoapp.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.navarromanuel.adescoapp.R;
 import com.navarromanuel.adescoapp.entidades.Parcela;
 import com.navarromanuel.adescoapp.interfaces.iComunicaFragments;
+import com.navarromanuel.adescoapp.ui.almacen.AlmacenFragment;
+import com.navarromanuel.adescoapp.ui.cuaderno.CuadernoFragment;
 import com.navarromanuel.adescoapp.ui.parcela.DetalleParcelaFragment;
+import com.navarromanuel.adescoapp.ui.parcela.ParcelaFragment;
+import com.navarromanuel.adescoapp.ui.perfil.PerfilFragment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
@@ -19,6 +27,8 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MenuActivity extends AppCompatActivity implements iComunicaFragments {
 
+    BottomNavigationView navView;
+    FrameLayout frameLayout;
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
     DetalleParcelaFragment detalleParcelaFragment;
@@ -26,15 +36,46 @@ public class MenuActivity extends AppCompatActivity implements iComunicaFragment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView = findViewById(R.id.nav_view);
+        frameLayout = findViewById(R.id.nav_host_fragment);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectListener);
+
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        /*AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_parcela, R.id.navigation_calendario, R.id.navigation_cuaderno, R.id.navigation_almacen, R.id.navigation_perfil)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        NavigationUI.setupWithNavController(navView, navController);*/
+    }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.navigation_almacen:
+                    setFragment(new AlmacenFragment());
+                    return true;
+                case R.id.navigation_cuaderno:
+                    setFragment(new CuadernoFragment());
+                    return true;
+                case R.id.navigation_parcela:
+                    setFragment(new ParcelaFragment());
+                    return true;
+                case R.id.navigation_perfil:
+                    setFragment(new PerfilFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
+        fragmentTransaction.commit();
     }
 
     @Override
