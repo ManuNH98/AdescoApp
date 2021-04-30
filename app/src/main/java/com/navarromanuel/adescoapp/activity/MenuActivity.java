3a7379ff3374544efd990e0ebcb6cb08c07758dc
Gabricv91 <gabricv9123@gmail.com>
2021-04-30 11:09:29 +0100

    el mensaje que me de la puta gana

--- app/src/main/java/com/navarromanuel/adescoapp/activity/MenuActivity.java
+++ app/src/main/java/com/navarromanuel/adescoapp/activity/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.navarromanuel.adescoapp.activity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.navarromanuel.adescoapp.R;
@@ -12,6 +14,22 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstaceState){
         super.onCreate(savedInstaceState);
         setContentView(R.layout.activity_menu);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.logo);
+       // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
+    /*@Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                //overridePendingTransition(R.anim.right_in, R.anim.right_out);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
 }
