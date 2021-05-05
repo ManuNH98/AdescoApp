1d5b42a943eeb0568267935dc48c2f79bf609e80
Manuel <manuelnhc@gmail.com>
2021-05-05 12:43:59 +0100

    menu commit

---
+++ app/src/main/java/com/navarromanuel/adescoapp/menu/InventarioActivity.java
@@ -0,0 +1,16 @@
+package com.navarromanuel.adescoapp.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.navarromanuel.adescoapp.R;
+
+public class InventarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventario);
+    }
+}
