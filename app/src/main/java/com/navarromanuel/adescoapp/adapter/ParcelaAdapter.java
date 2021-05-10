19460f3ff50ff82a7769f96874a73bcdd18dff5d
Manuel <manuelnhc@gmail.com>
2021-05-10 10:17:58 +0100

    menu commit

---
+++ app/src/main/java/com/navarromanuel/adescoapp/adapter/ParcelaAdapter.java
@@ -0,0 +1,4 @@
+package com.navarromanuel.adescoapp.adapter;
+
+public class ParcelaAdapter {
+}
--- app/src/main/java/com/navarromanuel/adescoapp/menu/ParcelaActivity.java
+++ app/src/main/java/com/navarromanuel/adescoapp/menu/ParcelaActivity.java
@@ -1,16 +1,23 @@
 package com.navarromanuel.adescoapp.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.navarromanuel.adescoapp.R;
+import com.navarromanuel.adescoapp.adapter.ParcelaAdapter;
 
 public class ParcelaActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private ParcelaAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcela);
+
+
     }
 }
